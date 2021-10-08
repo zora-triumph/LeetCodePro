db7838145bdbd9d8f3bf66737140489e64060cbe
zora-triumph <zora.triumph@gmail.com>
2021-10-08 11:11:13 +0800

    Day2 两数相加-递归法

--- No2AddTwoNum.java
+++ No2AddTwoNum.java
@@ -1,45 +1,93 @@
 public class No2AddTwoNum {
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode outNode = new ListNode();
-        ListNode head3 = outNode;
-        int val, c = 0;
-        while( l1 != null && l2 != null){
-            val = l1.val + l2.val + c;
-            c = val / 10;
-            val = val % 10;
-            outNode.next = new ListNode(val);
-            outNode = outNode.next;
+    // 迭代法
+    public static ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+        int total = 0;
+        int next1 = 0;
+        ListNode result = new ListNode();
+        // 辅助指针，在result链表上移动，最后只需要返回result->next
+        /* result  
+        *    head -> 0  ->  1  ->  2
+        *  current一直移动
+        */
+        ListNode current = result;
+        while(l1!=null && l2!=null){
+            total = l1.val + l2.val + next1;
+            // 新增一个节点
+            current.next = new ListNode(total%10);
+            next1 = total / 10 ;
             l1 = l1.next;
             l2 = l2.next;
+            current = current.next;
         }
-        while (l1 != null){
-            val = l1.val + c;
-            c = val / 10;
-            val = val % 10;
-            outNode.next = new ListNode(val);
-            outNode = outNode.next;
+        // l1没有走完
+        while(l1!=null){
+            total = l1.val + next1;
+            current.next = new ListNode(total%10);
+            next1 = total / 10;
             l1 = l1.next;
+            current = current.next;
         }
-        while (l2 != null){
-            val = l2.val + c;
-            c = val / 10;
-            val = val % 10;
-            outNode.next = new ListNode(val);
-            outNode = outNode.next;
+        // l2没有走完
+        while(l2!=null){
+            total = l2.val + next1;
+            current.next = new ListNode(total%10);
+            next1 = total / 10;
             l2 = l2.next;
+            current = current.next;
         }
-        if (c != 0){
-            outNode.next = new ListNode(c);
+        // 判断最后一位是否需要进位,若进位则需要新加一个节点
+        if(next1 != 0){
+            current.next = new ListNode(next1);
         }
-
-        return head3.next;
+        // 由于链表存储是从head之后存储的，可以将result看作head
+        return result.next;
     }
+
+
+    // 递归法
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        int total = 0;
+        int next1 = 0;
+        ListNode res = new ListNode();
+        /*
+        *  l1            l2               next1          total        res
+        *   9             9                 1             18           8
+        *   9(10)         9                 1             19           9
+        *   9(10)        (0)                1             10           0
+        *  (0)(1)        (0)                0             1            1
+        *
+        */
+        total = l1.val + l2.val;
+        next1 = total / 10;
+        res = new ListNode(total%10);
+        // 当全为0的时候，递归完成
+        if(l1.next!=null || l2.next!=null || next1!=0){
+            if(l1.next!=null){
+                l1 = l1.next;
+            }
+            else{
+                // 若l1走完了，则补0
+                l1 = new ListNode(0); 
+            }
+            if(l2.next!=null){
+                l2 = l2.next;
+            }
+            else{
+                // 若l2走完了，则补0
+                l2 = new ListNode(0);
+            }
+            // 把进位的值加到l1或者l2，实现递归
+            l1.val = l1.val + next1;
+            res.next = addTwoNumbers2(l1,l2);
+        }
+        // 递归完成之后，返回整个链表即可，此处没有head
+        return res;
+    }
+
+
+
 
     public static void main(String[] args){
-        
-
-
-
-
     }
+
 }
